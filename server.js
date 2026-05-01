49d2af037053c6bef77a1a1eed32c4bd90a96566
Victory Nwankwo <victorynwankwo162@gmail.com>
2026-05-01 08:26:04 -0400

    backend journey

---
+++ server.js
@@ -0,0 +1,14 @@
+// const os = require("os")
+const path = require("path")
+
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
+
+
+// console.log(__dirname)
+// console.log(__filename)
+
+console.log(path.dirname(__filename))
+console.log(path.basename(__filename))
+console.log(path.extname(__filename))
